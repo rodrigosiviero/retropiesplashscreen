45fd5546df3d353e118241dcea4bb491643be4b7
Rodrigo Campos <rodrigosiviero@gmail.com>
2014-12-19 17:32:40 -0200

    nah2

--- setup_splash.sh
+++ setup_splash.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 
+
+Boot_install () {
+   echo "Copying script to init.d"
+   cp splashscreen.sh /etc/init.d/
+   echo "Update-rc.d splashscreen.sh"
+   update-rc.d splashscreen.sh defaults
+   echo "Please do: sudo shutdown -r now"
+}
+
+Update () {
+   echo "Updating images and script"
+   git pull --rebase https://github.com/sur0x/retropiesplashscreen.git 
+}
+
 ####
-git clone git@github.com:sur0x/retropiesplashscreen.git /home/pi/additional/splashscreen
+PS3='Select the Option: '
+options=("Boot Install" "Update" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Boot Install")
+	    Boot_install
+	    break	
+            ;;
+        "Update")
+            Update
+	    break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
--- splashscreen.sh
+++ splashscreen.sh
@@ -1,3 +1,11 @@
+### BEGIN INIT INFO
+# Provides:          splashcreen
+# Required-Start:    
+# Required-Stop:     
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: splashscreen randomizer 
+### END INIT INFO
 #/bin/bash
 for path in /home/pi/RetroPie-Setup/supplementary/splashscreens/*; do
     [ -d "${path}" ] || continue # if not a directory, skip
