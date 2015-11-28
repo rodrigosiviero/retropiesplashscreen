dc99b4d92bb15f47be0b7239b59575c68b2468f2
Rodrigo Campos <sur0x@users.noreply.github.com>
2015-11-28 00:43:48 -0200

    CP -r *
    fixing

--- setup_splash.sh
+++ setup_splash.sh
@@ -27,7 +27,7 @@ elif [ $OUT == 128 ]; then
    rm -rf  retropiesplashscreen-master/
    rm tmp.zip
 fi
-sudo cp -r splashscreens/ /opt/retropie/supplementary/splashscreen
+sudo cp -r splashscreens/* /opt/retropie/supplementary/splashscreen/
 }
 
 ####
